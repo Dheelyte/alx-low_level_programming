334e369e1c2c8eb922e8ef6aa0a44aecddd4f540
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2022-09-09 13:47:44 -0700

    10. Inventing is a

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Combination
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 < 10; n1++)
+	{
+		for (n2 = 0; n2 < 10; n2++)
+		{
+			if (n1 < n2)
+			{
+				putchar((n1 % 10) + '0');
+				putchar((n2 % 10) + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
