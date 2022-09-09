3db7c1ee7cd1966faf571e15d479000537775eed
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2022-09-09 12:11:47 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Hexadecimal
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	char l;
+
+	for (i = 0; i < 10; i++)
+		putchar(i % 10 + '0');
+	for (l = 'a'; l <= 'f'; l++)
+		putchar(l);
+	putchar('\n');
+	return (0);
+}
