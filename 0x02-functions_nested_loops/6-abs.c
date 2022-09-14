f7045e0b8c4614066f30b368cbc0beef95236318
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2022-09-14 10:35:34 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * @n: number to be checked
+ * Return: Absolute value
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n)
+	else
+		return (n)
+}
