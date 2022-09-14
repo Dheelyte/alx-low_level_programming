2af5ba85788a4d652d25545a3df483acc110bc63
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2022-09-14 08:53:32 -0700

    10x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char a;
+
+	int n;
+
+	a = 'a';
+	a = 0;
+
+	while (n < 10)
+	{
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		};
+		n++;
+	}
+}
