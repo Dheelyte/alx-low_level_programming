a25b35145f991eb6c45537f99a20be6ad6e5e0cb
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2022-09-14 12:55:00 -0700

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,9 +8,19 @@
 
 void print_to_98(int n)
 {
-	while (n <= 98)
+	while (n <= 98 || n >= 98)
 	{
-		printf("%d, ", n);
-		n++;
+		printf("%d", n);
+		if (n > 98)
+		{
+			printf(", ");
+			n--;
+		}
+
+		if (n != 98)
+		{
+			printf(", ");
+			n++;
+		}
 	}
 }
