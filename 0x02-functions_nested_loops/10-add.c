4e93f0387684602313f8d337ec1eaf4f3678a3e7
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2022-09-14 12:28:59 -0700

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @a: first integer
+ * @b: second integer
+ * Return: the addition of a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
