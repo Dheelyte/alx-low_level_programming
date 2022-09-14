96c5e03007e17934b158287ec02e1b605af3cb86
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2022-09-14 12:00:13 -0700

    24 hrs

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ * Return - 24 hours
+ */
+
+void jack_bauer(void)
+{
+	int hr, min;
+
+	hr = 0;
+
+	while (hr < 24)
+	{
+		min = 0;
+		while (min < 60)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+			min++;
+		};
+		hr++;
+	}
+	return (0);
+}
