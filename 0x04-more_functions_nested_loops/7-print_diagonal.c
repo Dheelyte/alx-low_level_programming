c463cd5c193e111f49a85e5acf342006a2137d71
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2022-09-16 11:48:08 -0700

    Second deadline 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Entry point
+ * @n: number to be checked
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+			{
+				for (j = 0; j < n; j++)
+				{
+					if (i == j)
+					{
+						_putchar('\\');
+					}
+					else if (i > j)
+					{
+						_putchar(' ');
+					}
+				}
+				_putchar('\n');
+			}
+	}
+}
