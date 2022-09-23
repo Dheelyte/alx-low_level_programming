664dbf958102d6b5734d1b9341b0a49474c801a2
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2022-09-23 14:23:53 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strcat - Entry point
+ * @dest: destination string
+ * @src: string to append
+ * Return: pointer to the resulting string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
