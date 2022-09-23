9c638981e1058d30b332ad0e33e22829efe31cd7
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2022-09-23 15:24:18 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_array - Entry point
+ * @a: array
+ * @n: number of elements in array
+ * Return: reversed array
+ */
+
+void reverse_array(int *a, int n)
+{
+	for (i = n; i > 0; i--)
+		a[i] = 
+}
