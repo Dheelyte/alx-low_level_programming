3dcd468e852c117c1d0be66204dd24d8e76cb996
Dheelyte <64509158+Dheelyte@users.noreply.github.com>
2022-09-27 23:07:36 +0100

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 
 int _putchar(char c);
@@ -9,3 +12,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+
+#endif
