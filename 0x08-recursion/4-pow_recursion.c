8a09dee09df32f2a71978d8e1f9fc98f77eb6fa1
Olu-Olagbuji Delight <olagbujidelight@gmail.com>
2022-09-30 11:35:45 -0700

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Print x power y
+ * @x: first number
+ * @y: second number
+ * Return: exponential
+ */
+
+int _pow_recursion(int x, int y)
+{
+	return (x ** y);
+}
